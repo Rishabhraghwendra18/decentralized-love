4090deadcc826fecf5a1071dcc97c5392c966796
Rishabhraghwendra18 <rishabhraghwendra2002@gmail.com>
2022-01-13 15:39:52 +0530

    added the Rinkeby Testnet Only badge

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,25 +1,31 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import "../Hero.css";
 
-export default function Navbar({provider}) {
-      const [walletAddress,setWalletAdress] = useState(null);
-      useEffect(async () => {
+export default function Navbar({ provider }) {
+  const [walletAddress, setWalletAdress] = useState(null);
+  useEffect(async () => {
+    setUserWalletAddress();
+  }, []);
+  async function setUserWalletAddress() {
+    setWalletAdress(await provider.getSigner().getAddress());
+  }
+  return (
+    <nav>
+      <div className="absolute top-2 right-2 flex flex-row items-center gap-2 flex-wrap-reverse">
+        <div className="p-1 border-2 border-[#afafaf] bg-[#afafaf] text-[#4c4a4a] text-xs rounded-full">
+          Rinkeby Testnet Only
+        </div>
+        <button
+          onClick={async () => {
+            await provider.send("eth_requestAccounts", []);
+            const signer = await provider.getSigner();
             setUserWalletAddress();
-      }, [])
-      async function setUserWalletAddress() {
-           setWalletAdress(await provider.getSigner().getAddress());
-      }
-      return (
-            <nav>
-                  <button onClick={async ()=>{
-                        await provider.send("eth_requestAccounts", []);
-                        const signer = await provider.getSigner();
-                        setUserWalletAddress();
-                  }}
-                  className="w-32 absolute top-2 right-2 p-1 text-white rounded-lg border-4 border-indigo bg-indigo text-ellipsis overflow-hidden shadow-lg hover:shadow-indigo"
-                  >
-                        {walletAddress ? walletAddress:'Connect Wallet'}
-                  </button>
-            </nav>
-      );
+          }}
+          className="w-32 p-1 text-white rounded-full border-4 border-indigo bg-indigo text-ellipsis overflow-hidden shadow-lg hover:shadow-indigo"
+        >
+          {walletAddress ? walletAddress : "Connect Wallet"}
+        </button>
+      </div>
+    </nav>
+  );
 }
