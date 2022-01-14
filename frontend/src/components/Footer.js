dbdf6f068905feefb91259ed71148d9e26e73b32
Rishabhraghwendra18 <rishabhraghwendra2002@gmail.com>
2022-01-14 13:32:48 +0530

    removed re=noreferrer warning

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,10 +3,10 @@ export default function Footer() {
       return(
             <footer className="fixed bottom-0 p-3 bg-pink w-full flex justify-center">
                   <p>
-                  Made by <a href="https://www.linkedin.com/in/rishabh-sde/" target="_blank"
+                  Made by <a href="https://www.linkedin.com/in/rishabh-sde/" target="_blank" rel="noreferrer"
                   className="underline text-blue-600 hover:text-[#0000FF] visited:text-indigo"
                   >Rishabh Raghwendra</a> ♥️
-                  | <a href="https://github.com/Rishabhraghwendra18/decentralized-love" target="_blank" className="underline text-blue-600 hover:text-[#0000FF] visited:text-indigo">GitHub Repo</a>
+                  | <a href="https://github.com/Rishabhraghwendra18/decentralized-love" target="_blank" rel="noreferrer" className="underline text-blue-600 hover:text-[#0000FF] visited:text-indigo">GitHub Repo</a>
                   </p>
             </footer>
       );
